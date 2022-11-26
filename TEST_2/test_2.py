90e569c1e9481cd3222582ce9bf686527d67bd30
alexiskaldany <alexiskaldany@gmail.com>
2022-11-26 18:06:22 -0500

    MLP working!!!

--- TEST_2/test_2.py
+++ TEST_2/test_2.py
@@ -21,35 +21,41 @@ target_column = ""
 
 # train_dataframe = 
 class CustomDataset(Dataset):
-    def __init__(self, dataframe, input_column, target_column,max_len):
+    def __init__(self, dataframe, input_column, target_column, max_len):
         self.dataframe = dataframe
         self.input_column = input_column
         self.target_column = target_column
         self.label_dictionary = {label: i for i, label in enumerate(self.dataframe[self.target_column].unique())}
         self.max_len = max_len
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.tokenized_input = self.dataframe[self.input_column].apply((lambda x: self.tokenizer.encode(x, add_special_tokens=True, max_length = self.max_len, truncation = True, padding = True, return_tensors = 'pt')))
-        self.get_one_hot_labels_list = lambda label: [1 if i == self.label_dictionary[label] else 0 for i in range(len(self.label_dictionary))]
+        self.tokenized_input = self.dataframe[self.input_column].apply((lambda x: self.tokenizer.encode(x, add_special_tokens=True, max_length = self.max_len, truncation = True, padding = "max_length", return_tensors = 'pt')))
+        self.one_hot_labels = self.dataframe[self.target_column].apply((lambda x: self.one_hot_encode_label(x)))
         """ 
         Create a one-hot encoding for each label. Turn into a tensor. self.one_hot_labels is a list of tensors.
         """
-        
+    def one_hot_encode_label(self, label):
+        one_hot = torch.zeros(len(self.label_dictionary))
+        one_hot[self.label_dictionary[label]] = 1
+        return one_hot
     def __len__(self):
         return len(self.dataframe)
     
     def __getitem__(self, index):
-        return self.tokenized_input[index], torch.tensor(self.get_one_hot_labels_list(self.dataframe[self.target_column][index]))
+        return self.tokenized_input[index], self.one_hot_labels[index]
 
 
 MAX_LEN = 252
 HIDDEN_SIZE = 768
-NUM_EPOCHS = 10
+NUM_EPOCHS = 1000
 PRINT_LOSS_EVERY = 10
 train_dataframe = pd.read_csv("/Users/alexiskaldany/school/nlp/TEST_2/covid.csv").rename(columns={"content": "input", "category": "target"})
 labels = train_dataframe['target'].unique()
 NUM_OF_CLASSES = len(labels)
 train_dataset = CustomDataset(train_dataframe, input_column = "input", target_column = "target", max_len = MAX_LEN)
-print(train_dataset[0][0].shape)
+# print(train_dataset[0][0].shape)
+# print(train_dataset[0][0].float().squeeze(0).shape)
+print(len(train_dataset.tokenized_input))
+print(len(train_dataset.one_hot_labels))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 """ 
 MLP
@@ -69,11 +75,8 @@ class MLP(nn.Module):
     """
     def __init__(self, input_size, hidden_size, output_size):
         super(MLP, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.linear1 = nn.Linear(self.input_size, self.hidden_size)
-        self.linear2 = nn.Linear(self.hidden_size, self.output_size)
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
     def forward(self, x):
         x = self.linear1(x)
@@ -81,24 +84,27 @@ class MLP(nn.Module):
         x = self.linear2(x)
         return x
 
-model = MLP(MAX_LEN, MAX_LEN, NUM_OF_CLASSES).to(device)
+model = MLP(MAX_LEN, HIDDEN_SIZE, NUM_OF_CLASSES).to(device)
 print(model)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-criterion = nn.CrossEntropyLoss()
+loss_function = nn.BCEWithLogitsLoss()
 
 """ Train the MLP"""
+length_of_dataset = len(train_dataset)
+print(length_of_dataset)
 
 for epoch in range(NUM_EPOCHS):
-    for index, batch in enumerate(train_dataset):
-        input = torch.tensor(batch[0][0]).float().to(device)
-        target = batch[1].to(device)
-        optimizer.zero_grad()
+    for i in range (length_of_dataset):
+        input, target = train_dataset.__getitem__(i)
+        input = input.float().squeeze(0).to(device)
+        target = target.to(device)
         output = model(input)
-        loss = criterion(output, target)
+        loss = loss_function(output, target)
         loss.backward()
         optimizer.step()
-        if index % PRINT_LOSS_EVERY == 0:
-            print(f"Epoch: {epoch} Loss: {loss.item()}")
+        if i % PRINT_LOSS_EVERY == 0:
+            print(f"Epoch: {epoch}, Loss: {loss}")
+        
 
 """
 RNN
